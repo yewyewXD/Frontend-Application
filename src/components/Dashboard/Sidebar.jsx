6971df3813c9a7dde7ee84ef847a48a3c7e6e2cb
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-15 16:11:35 +0800

    style sidebar content & add avatar

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import profileAvatar from "../../images/avatar.jpg";
+
+const SidebarContentContainer = styled.div`
+  height: 83%;
+  border-bottom: solid 1px lightgrey;
+  overflow-y: scroll;
+`;
+
+const SidebarContent = styled.div``;
+
+const SidebarFooter = styled.div`
+  height: 17%;
+  padding: 1rem 0;
+`;
+
+const SidebarProfile = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 90px;
+    width: 90px;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+`;
 
 export default function Sidebar() {
-  return <div className="dashboard-sidebar">hi from sidebar</div>;
+  return (
+    <div className="dashboard-sidebar">
+      <SidebarContentContainer className="sidebar-content-container">
+        <SidebarContent className="sidebar-content">
+          <SidebarProfile className="sidebar-profile">
+            <img src={profileAvatar} alt="" />
+            <button className="btn btn-primary btn-sm">Login</button>
+          </SidebarProfile>
+        </SidebarContent>
+      </SidebarContentContainer>
+
+      <SidebarFooter className="sidebar-footer">footer</SidebarFooter>
+    </div>
+  );
 }
