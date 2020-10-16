c093f6ae447fa46f2951cd7e78352b63bbd13a40
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-16 11:32:51 +0800

    add main dashboard section

--- src/components/Dashboard/Feed.jsx
+++ src/components/Dashboard/Feed.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const FeedHeading = styled.h1`
+  font-size: 22px;
+  margin: 0 0 15px 0;
+`;
+
+const FeedSubheading = styled.div`
+  font-size: 16px;
+  margin: 0 0 15px 0;
+  font-weight: 300;
+`;
 
 export default function Feed() {
   return (
-    <div
-      className="dashboard-feed"
-      style={{ height: "5rem", background: "grey" }}
-    >
-      feed
+    <div className="dashboard-feed">
+      <FeedHeading className="feed-heading">User Profile Setting</FeedHeading>
+
+      <FeedSubheading className="feed-subheading">
+        Edit you account and personal information here
+      </FeedSubheading>
     </div>
   );
 }
