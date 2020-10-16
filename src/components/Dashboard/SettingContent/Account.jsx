cb8e6287583d08066419998b8931a30e9f535716
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-16 14:45:56 +0800

    init form handling

--- src/components/Dashboard/SettingContent/Account.jsx
+++ src/components/Dashboard/SettingContent/Account.jsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AccountSetting() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+
+  function submitAccountSetting(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="account-setting">
+    <form className="account-setting" onSubmit={submitAccountSetting}>
       <div className="form-group">
         <label htmlFor="email" className="form-label">
           <b>Email</b>
         </label>
-        <input type="text" className="form-input" />
+        <input
+          type="text"
+          className="form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor="password" className="form-label">
           <b>Password</b>
         </label>
-        <input type="password" className="form-input" />
+        <input
+          type="password"
+          className="form-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor="passwordRepeat" className="form-label">
           <b>Password Repeat</b>
         </label>
-        <input type="password" className="form-input" />
+        <input
+          type="password"
+          className="form-input"
+          value={passwordRepeat}
+          onChange={(e) => setPasswordRepeat(e.target.value)}
+        />
       </div>
 
       <button className="btn btn-primary btn-md">Save Changes</button>
--- src/components/Dashboard/SettingContent/User.jsx
+++ src/components/Dashboard/SettingContent/User.jsx
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function UserSetting() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [country, setCountry] = useState("");
+
+  function submitUserSetting(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="user-setting">
+    <form className="user-setting" onSubmit={submitUserSetting}>
       <div className="form-group">
         <label htmlFor="firstName" className="form-label">
           <b>First Name</b>
         </label>
-        <input type="text" className="form-input" />
+        <input
+          type="text"
+          className="form-input"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor="lastName" className="form-label">
           <b>Last Name</b>
         </label>
-        <input type="text" className="form-input" />
+        <input
+          type="text"
+          className="form-input"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor="address" className="form-label">
           <b>Address</b>
         </label>
-        <input type="text" className="form-input" />
+        <input
+          type="text"
+          className="form-input"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor="country" className="form-label">
           <b>Country</b>
         </label>
-        <input type="text" className="form-input" />
+        <input
+          type="text"
+          className="form-input"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
       </div>
 
       <button className="btn btn-primary btn-md">Save Changes</button>
