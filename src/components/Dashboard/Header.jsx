b839cc7eedbf518c6e3d9fec0648500e375cbda1
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-17 14:15:06 +0800

    responsive to 1100px screen width

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -4,6 +4,7 @@ import logo from "../../images/innoloft_white_logo.svg";
 import { GoSearch } from "react-icons/go";
 import { CgChevronDown } from "react-icons/cg";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import { VscThreeBars } from "react-icons/vsc";
 import profileAvatar from "../../images/avatar.jpg";
 
 const Navbar = styled.nav`
@@ -22,13 +23,14 @@ const NavbarContainer = styled.div`
   @media (max-width: 1700px) {
     padding: 0.5rem 10rem;
   }
-
   @media (max-width: 1500px) {
     padding: 0.5rem 5rem;
   }
-
   @media (max-width: 1300px) {
     padding: 0.5rem 2rem;
+  }
+  @media (max-width: 1100px) {
+    padding: 0.5rem 3rem;
   }
 `;
 
@@ -42,10 +44,13 @@ const NavbarBrand = styled.a`
   }
 `;
 
-const NavbarToggler = styled.button`
+const NavbarToggler = styled.span`
   display: none;
+  margin-right: 20px;
+  color: white;
+  font-size: 1.5rem;
 
-  @media (max-width: 1024px) {
+  @media (max-width: 1100px) {
     display: flex;
   }
 `;
@@ -58,17 +63,20 @@ const NavSearch = styled.form`
   margin: 0 30px;
 
   .search-input {
-    padding: 0.4rem 0.6rem;
+    padding: 0.4rem 2.2rem 0.4rem 0.6rem;
     width: 100%;
     border-radius: 10px;
+  }
+  @media (max-width: 1100px) {
+    justify-content: center;
   }
 
   .search-btn {
     font-size: 18px;
     position: absolute;
-    margin: 5px 1rem 0 0;
+    margin: 5px 0.6rem 0 0;
     cursor: pointer;
-    background: #f4f4f4;
+    background: none;
   }
 `;
 
@@ -104,8 +112,8 @@ export default function Header() {
   return (
     <Navbar className="navbar bg-theme">
       <NavbarContainer className="navbar-container">
-        <NavbarToggler className="navbar-toggler" type="button">
-          button
+        <NavbarToggler className="navbar-toggler btn all-center" type="button">
+          <VscThreeBars />
         </NavbarToggler>
 
         <NavbarBrand className="navbar-brand" href="/">
