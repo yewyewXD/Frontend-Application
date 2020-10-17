31d31f8c2cd0b90b0a869c1845449c26b8c5239f
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-17 20:40:45 +0800

    add fake ajax request

--- src/redux/actions/UserProfile/index.js
+++ src/redux/actions/UserProfile/index.js
@@ -1,13 +1,16 @@
 import { UPDATE_ACCOUNT_INFO, UPDATE_USER_INFO } from "./types";
+import axios from "axios";
 
-export function updateAccountInfo(email, password) {
+export async function updateAccountInfo(email, password) {
+  await axios.post("/fakeApi/account", { email, password });
   return {
     type: UPDATE_ACCOUNT_INFO,
     payload: { email, password },
   };
 }
 
-export function updateUserInfo(firstName, lastName, address, country) {
+export async function updateUserInfo(firstName, lastName, address, country) {
+  await axios.post("/fakeApi/user", { firstName, lastName, address, country });
   return {
     type: UPDATE_USER_INFO,
     payload: { firstName, lastName, address, country },
