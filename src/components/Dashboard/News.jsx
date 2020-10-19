410b4a50a80e08f15da3b95a6bef9dbc36ffb668
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-19 14:41:13 +0800

    BEM style refactor: dashboard side content

--- src/components/Dashboard/News.jsx
+++ src/components/Dashboard/News.jsx
@@ -1,51 +1,15 @@
 import React from "react";
-import styled from "styled-components";
 import newsImage1 from "../../images/news_1.png";
-
-const NewsCard = styled.div`
-  border-radius: 5px;
-  background: white;
-  padding: 20px;
-  box-shadow: lightgrey 0px 0px 5px;
-
-  .news-heading {
-    font-size: 16px;
-    margin: 0 0 7px 0;
-    font-weight: 600;
-  }
-
-  .news-image {
-    width: 100%;
-  }
-
-  .news-title {
-    font-size: 16px;
-    margin: 10px 0;
-    font-weight: 600;
-  }
-
-  .news-article {
-    margin: 0 0 15px 0;
-  }
-
-  .news-read-more {
-    cursor: pointer;
-    color: grey;
-    &:hover {
-      color: #272e71;
-    }
-  }
-`;
 
 export default function News() {
   return (
     <div className="side-content">
-      <NewsCard className="news-card">
-        <div className="news-heading">Current Challenge</div>
-        <img src={newsImage1} alt="" className="news-image" />
-        <div className="news-title">Connected Care Challenge</div>
+      <div className="card">
+        <div className="card__heading">Current Challenge</div>
+        <img src={newsImage1} alt="" className="card__image" />
+        <div className="card__title">Connected Care Challenge</div>
 
-        <div className="news-article">
+        <div className="card__article">
           Philips wants to improve the lives of billions in the next years to
           come. When you connect data, technology and people seemingly
           impossible things become possible. Patient outcomes improve, hospital
@@ -53,7 +17,7 @@ export default function News() {
           healthcare better, together.
         </div>
 
-        <div className="news-article">
+        <div className="card__article">
           As we are all well aware, the healthcare landscape is changing
           rapidly. Increased complexity, patient volume, and pressure on devices
           and system integration demands new approaches and thinking in the
@@ -62,8 +26,8 @@ export default function News() {
           informatics through connected care is [...]
         </div>
 
-        <div className="news-read-more">Read more</div>
-      </NewsCard>
+        <div className="card__read-more">Read more</div>
+      </div>
     </div>
   );
 }
